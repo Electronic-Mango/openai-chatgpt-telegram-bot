b79e3d50be01cb4860621c4487fcf5bf21f49f92
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2023-05-11 16:24:42 +0200

    Add missing prompt commands

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
-from chat import initial_message, next_message, reset_conversation
+from chat import (get_custom_prompt, initial_message, next_message, remove_custom_prompt, remove_prompt,
+                  reset_conversation, store_custom_prompt)
 from rate_error_handler import rate_error_handler
 
 load_dotenv()
@@ -20,10 +21,24 @@ def main() -> None:
 
     start_handler = CommandHandler("start", start)
     application.add_handler(start_handler)
+
     talk_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), talk)
     application.add_handler(talk_handler)
+
     reset_handler = CommandHandler("reset", reset)
     application.add_handler(reset_handler)
+
+    prompt_set_handler = CommandHandler("promptset", prompt_set)
+    application.add_handler(prompt_set_handler)
+
+    prompt_reset_handler = CommandHandler("promptreset", prompt_reset)
+    application.add_handler(prompt_reset_handler)
+
+    prompt_get_handler = CommandHandler("promptget", prompt_get)
+    application.add_handler(prompt_get_handler)
+
+    prompt_remove_handler = CommandHandler("promptremove", prompt_remove)
+    application.add_handler(prompt_remove_handler)
 
     application.add_error_handler(rate_error_handler)
 
@@ -47,6 +62,35 @@ async def reset(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     reset_conversation(chat_id)
     await context.bot.send_message(chat_id=chat_id, text="Conversation restarted.")
     await start(update, context)
+
+
+async def prompt_set(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.effective_chat.id
+    reset_conversation(chat_id)
+    new_prompt = " ".join(context.args)
+    store_custom_prompt(chat_id, new_prompt)
+    await context.bot.send_message(chat_id=chat_id, text=f"Prompt set to: **{new_prompt}**")
+
+
+async def prompt_reset(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.effective_chat.id
+    reset_conversation(chat_id)
+    remove_custom_prompt(chat_id)
+    await context.bot.send_message(chat_id=chat_id, text="Prompt reset.")
+
+
+async def prompt_get(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.effective_chat.id
+    custom_prompt = get_custom_prompt(chat_id)
+    response = f"Prompt set to: **{custom_prompt}**" if custom_prompt else "No custom prompt configured."
+    await context.bot.send_message(chat_id=chat_id, text=response)
+
+
+async def prompt_remove(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.effective_chat.id
+    reset_conversation(chat_id)
+    remove_prompt(chat_id)
+    await context.bot.send_message(chat_id=chat_id, text="Prompt removed.")
 
 
 if __name__ == "__main__":
