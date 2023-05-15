007c1866e84ccdb1e5a093febada0507ad93ac0a
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2023-05-15 17:16:02 +0200

    Use rate limit error message from "chat" when handling errors

--- src/rate_error_handler.py
+++ src/rate_error_handler.py
@@ -1,8 +1,10 @@
 from openai.error import RateLimitError
 from telegram.ext import ContextTypes
 
+from chat import RATE_LIMIT_MESSAGE
+
 
 async def rate_error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
     error = context.error
-    message = "Rate limit reached, try again in 20s" if isinstance(error, RateLimitError) else error
+    message = RATE_LIMIT_MESSAGE if isinstance(error, RateLimitError) else error
     await context.bot.send_message(update.effective_chat.id, message)
